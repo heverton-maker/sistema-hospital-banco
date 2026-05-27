5005c05d7ae9b6465f2101a06efeaa656b4d97ba
heverton-maker <247903108+heverton-maker@users.noreply.github.com>
2026-05-27 20:17:34 -0300

    desifileirar e front
    todas funçoes prontas acredito eu

--- Fila.c
+++ Fila.c
@@ -18,7 +18,6 @@ void enfileirar(Fila *f, Pessoa nova_pessoa) { // Inserir no final
     }
     novo_no->dados = nova_pessoa; // Coloca os dados da pessoa nesse espaço
     novo_no->proximo = NULL; // Fim da fila
-   
     if (f->inicio == NULL) { // Fila vazia
         f->inicio = novo_no;
         f->fim = novo_no;
@@ -29,9 +28,27 @@ void enfileirar(Fila *f, Pessoa nova_pessoa) { // Inserir no final
 }
 
 
+void desenfileirar(Fila *f) { //  Remover da frente
+    if (f->inicio == NULL) {
+        printf("A fila ja esta vazia!\n");
+        return; // Sai da função para não dar Segmentation Fault
+    }  
+    No *aux = f->inicio; // Placa clone
+    f->inicio = aux->proximo; // A fila anda (placa oficial passa para o segundo da fila)
+    free(aux); // Libera a memória 
+    if (f->inicio == NULL) {
+        f->fim = NULL;  //Se a fila esvaziou, o fim também precisa ser NULL
+    }
+}
+
+
+Pessoa* front(Fila *f) { // FRONT: Consultar quem é o primeiro da fila 
+    if (f->inicio == NULL) {
+        return NULL; // Fila vazia, não há ninguém na frente
+    }
+
+    return &(f->inicio->dados); // Retorna o endereço da ficha da primeira pessoa
+}
 
 
 
-
-void desenfileirar (Fila *f)
-Pessoa* front(Fila *f)
--- Fila.h
+++ Fila.h
@@ -3,7 +3,7 @@
 
 // O MOLDE DOS DADOS Serve para Hospital e o Banco
 typedef struct {
-	int id; senha do banco ou registro do hospital
+	int id; //senha do banco ou registro do hospital
 	char nome[50];
 	int prioridade;
 }Pessoa;
@@ -13,13 +13,15 @@ typedef struct no {
 	struct no *proximo;
 }No;
 
-tydef struct{
-	No *inicio
-	No *fim
+typedef struct{
+	No *inicio;
+	No *fim;
 }Fila;
 
 
 void iniciar_fila(Fila *f);
-void enfileirar (Fila *f, Pessoa nova_pessoa)
-void desenfileirar (Fila *f)
-Pessoa* front(Fila *f)
+void enfileirar (Fila *f, Pessoa nova_pessoa);
+void desenfileirar (Fila *f);
+Pessoa* front(Fila *f);
+
+#endif
