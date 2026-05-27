214f2d0b2829cf6fe7edab5ace641ec83b52b18e
heverton-maker <247903108+heverton-maker@users.noreply.github.com>
2026-05-26 21:08:14 -0300

    Fila.h
    os structs Serve para Hospital e o Banco (eu acho não testei)

---
+++ Lista.h
@@ -0,0 +1,25 @@
+#ifndef FILA_H
+#define FILA_H 
+
+// O MOLDE DOS DADOS Serve para Hospital e o Banco
+typedef struct {
+	int id; senha do banco ou registro do hospital
+	char nome[50];
+	int prioridade;
+}Pessoa;
+
+typedef struct no {
+	Pessoa dados;
+	struct no *proximo;
+}No;
+
+tydef struct{
+	No *inicio
+	No *fim
+}Fila;
+
+
+void iniciar_fila(Fila *f);
+void enfileirar (Fila *f, Pessoa nova_pessoa)
+void desenfileirar (Fila *f)
+Pessoa* front(Fila *f)
